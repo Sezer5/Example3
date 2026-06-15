b0c37f0fa2e330fe8e182919840ca989c365d544
Sezer Ünalmış <76732537+Sezer5@users.noreply.github.com>
2026-06-15 17:09:30 +0300

    SF

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,16 +3,29 @@
         <h4 class="text-white fw-bold"><i class="bi bi-cpu-fill me-2"></i>AdminPanel</h4>
     </div>
     <ul class="nav flex-column">
-        <li class="nav-item"><a href="{{ route('admin.keyword.index') }}" class="nav-link active"><i
-                    class="bi bi-key me-2"></i>
-                Keywords</a></li>
-        <li class="nav-item"><a href="{{ route('admin.article.index') }}" class="nav-link"><i
-                    class="bi bi-file-earmark"></i> Articles</a></li>
         <li class="nav-item">
-            <a href="{{ route('admin.novel.index') }}" class="nav-link">
-                <i class="bi bi-book"></i> Novels</a>
+            <a href="{{ route('admin.home') }}" class="nav-link {{ Route::is('admin.home') ? 'active' : '' }}">
+                <i class="bi bi-house me-2"></i> Home
+            </a>
         </li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-people me-2"></i> Kullanıcılar</a></li>
-        <li class="nav-item"><a href="#" class="nav-link"><i class="bi bi-gear me-2"></i> Ayarlar</a></li>
+        <li class="nav-item">
+            <a href="{{ route('admin.keyword.index') }}"
+                class="nav-link {{ Route::is('admin.keyword.*') ? 'active' : '' }}">
+                <i class="bi bi-key me-2"></i> Keywords
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('admin.article.index') }}"
+                class="nav-link {{ Route::is('admin.article.*') ? 'active' : '' }}">
+                <i class="bi bi-file-earmark"></i> Articles
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{ route('admin.novel.index') }}"
+                class="nav-link {{ Route::is('admin.novel.*') ? 'active' : '' }}">
+                <i class="bi bi-book"></i> Novels
+            </a>
+        </li>
+
     </ul>
 </nav>
